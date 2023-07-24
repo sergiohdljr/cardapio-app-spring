37c45c4165d1209f7a4378451f09c9e4d2c9c1fd
sergiohdljr <sergio.tjf1@gmail.com>
2023-07-24 11:04:58 -0300

    refactor: create service and add to controller

--- src/main/java/com/example/cardapio/controller/FoodController.java
+++ src/main/java/com/example/cardapio/controller/FoodController.java
@@ -3,38 +3,34 @@ package com.example.cardapio.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.cardapio.food.Food;
-import com.example.cardapio.food.FoodRequestDTO;
-import com.example.cardapio.food.FoodResponseDTO;
-import com.example.cardapio.food.foodRepository;
+import com.example.cardapio.service.FoodService;
 
 @RestController
 @RequestMapping("food")
 public class FoodController {
 
     @Autowired
-    foodRepository repository;
+    FoodService service;
 
     @CrossOrigin(origins = "*", allowedHeaders = "*")
     @GetMapping
-    public List<FoodResponseDTO> getAll() {
-
-        List<FoodResponseDTO> foodList = repository.findAll().stream().map(FoodResponseDTO::new).toList();
-        return foodList;
+    public ResponseEntity<List<Food>> getAll() {
+        return new ResponseEntity<>(this.service.getAllFood(), HttpStatus.OK);
     }
 
-    @CrossOrigin(origins = "*", allowedHeaders = "*")
-    @PostMapping
-    public void saveFood(@RequestBody FoodRequestDTO data) {
-        Food foodData = new Food(data);
-        repository.save(foodData);
-        return;
-    }
+    // @CrossOrigin(origins = "*", allowedHeaders = "*")
+    // @PostMapping
+    // public void saveFood(@RequestBody FoodRequestDTO data) {
+    // Food foodData = new Food(data);
+    // repository.save(foodData);
+    // return;
+    // }
 }
---
+++ src/main/java/com/example/cardapio/service/FoodService.java
@@ -0,0 +1,20 @@
+package com.example.cardapio.service;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.cardapio.food.Food;
+import com.example.cardapio.food.foodRepository;
+
+@Service
+public class FoodService {
+
+    @Autowired
+    foodRepository repository;
+
+    public List<Food> getAllFood() {
+        List<Food> foodList = repository.findAll();
+        return foodList;
+    }
+}
